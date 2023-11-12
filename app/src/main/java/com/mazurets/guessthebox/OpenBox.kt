9beb3e33a9cdad82df8f4ca5a2638739ef1e7fa5
theri <therigasmazurets@gmail.com>
2023-11-12 16:34:28 +0200

    add openbox class

---
+++ app/src/main/java/com/mazurets/guessthebox/OpenBox.kt
@@ -0,0 +1,11 @@
+package com.mazurets.guessthebox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpenBox : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_box)
+    }
+}
